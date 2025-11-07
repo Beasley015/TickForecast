a60d95d327442f23b854452f23dbd41bbd0dc7a3
E. Beasley <beasley.em@gmail.com>
2025-11-07 12:37:39 -0500

    update neon data download

--- R/neon_download.R
+++ R/neon_download.R
@@ -1,9 +1,11 @@
-Sys.setenv("NEONSTORE_HOME" = "/projectnb/dietzelab/fosterj/Data/neonstore")
-Sys.setenv("NEONSTORE_DB" = "/projectnb/dietzelab/fosterj/Data/neonstore")
+# Sys.setenv("NEONSTORE_HOME" = "/projectnb/dietzelab/fosterj/Data/neonstore")
+# Sys.setenv("NEONSTORE_DB" = "/projectnb/dietzelab/fosterj/Data/neonstore")
+
+setwd("./Data")
 
 library(neonstore)
 
-neon_download("DP1.10093.001")
+neon_download("DP1.10093.001", dir = getwd())
 
 neon_store()
 tck_taxonomyProcessed <- neon_table("tck_taxonomyProcessed-basic")
