fdaac7742219cb40d6e538bff7f002809db680fd
Emily Beasley <beasley.em@gmail.com>
2026-02-13 08:11:58 -0500

    Update writeTransferSamples.R

--- R/writeTransferSamples.R
+++ R/writeTransferSamples.R
@@ -6,59 +6,16 @@ library(utils)
 library(ggpubr)
 library(MetBrewer)
 
-dir.top <- "/projectnb/dietzelab/fosterj"
-dir.out <- file.path(dir.top, "FinalOut/Chapter3/outConstraintForestUpdate")
-dir.analysis <-  file.path(dir.top, "FinalOut/Chapter3/analysisConstraintForestUpdate")
+dir.top <- getwd()
+dir.out <- file.path(dir.top, "out")
+dir.analysis <-  file.path(dir.top, "analysis")
 if(!dir.exists(dir.analysis)) dir.create(dir.analysis, recursive = TRUE, showWarnings = FALSE)
-
-
-jobs <- c(
-  "/ic/",
-  "/ic_parameter/",
-  "/ic_parameter_driver/",
-  "/ic_parameter_driver_process/"  
-)
-
-job.num <- as.numeric(Sys.getenv("SGE_TASK_ID"))
-if(is.na(job.num)) job.num <- 3
 
 out.files <- list.files(dir.out, recursive = TRUE)
 process.samples <- grep("stateSamples.csv", out.files, value = TRUE)
-quantScore <- grep(jobs[job.num], process.samples, value = TRUE) 
-
-
-jobnames <- c(
-  "ic",
-  "ic_parameter",
-  "ic_parameter_driver",
-  "ic_parameter_driver_process"  
-)
-
-job.sub <- jobnames[job.num]
-
-find_ua <- function(x){
-  if(grepl("ic", x)){
-    ua <- "IC"
-  } 
-  if(grepl("ic_parameter", x)){
-    ua <- "+ Parameter"
-  } 
-  if(grepl("ic_parameter_driver", x)){
-    ua <- "+ Driver"
-  } 
-  if(grepl("ic_parameter_driver_process", x)){
-    ua <- "+ Process"
-  }
-  ua
-}
+rm(out.files)
 
 find_model <- function(x){
-  if(grepl("Static", x)){
-    m <- "Static"
-  }
-  if(grepl("WithMNAMice", x)){
-    m <- "Mice"
-  } 
   if(grepl("Weather", x)){
     m <- "Weather"
   } 
@@ -75,44 +32,52 @@ find_species <- function(x){
   species
 }
 
-df.process <- tibble()
-for(i in seq_along(quantScore)){
-  dfi <- read_csv(file.path(dir.out, quantScore[i])) %>% 
-    suppressMessages()
-  st <- str_extract(quantScore[i], "\\d{4}-\\d{2}-\\d{2}")
-  spp <- find_species(quantScore[i])
-  m <- find_model(quantScore[i])
-  u <- find_ua(quantScore[i])
+sites <- c("BLAN","HARV","KONZ","LENO","OSBS","SCBI","SERC","TALL","TREE","UKFS",
+           "GREN","HNRY","TEA")
+
+
+for(j in 1:length(sites)){
+  # Blank df for each site
+  df.process <- tibble()
   
-  df.summary <- dfi %>% 
-    # group_by(node, ua, model, siteID) %>% 
-    group_by(time, lifeStage, siteID) %>%
-    summarise(lower95 = quantile(value, 0.025),
-              lower75 = quantile(value, 0.125),
-              median = median(value),
-              mean = mean(value),
-              upper75 = quantile(value, 0.875),
-              upper95 = quantile(value, 0.975), 
-              variance = var(value)) %>% 
-    mutate(model = m,
-           ua = u,
-           species = spp,
-           start.date = st) %>% 
+  # extract files for a particular site
+  quantScore <- grep(sites[j], process.samples, value = T)
+  
+  for(i in seq_along(quantScore)){
+    dfi <- read_csv(file.path(dir.out, quantScore[i])) %>% 
+      suppressMessages()
+    
+    st <- str_extract(quantScore[i], "\\d{4}-\\d{2}-\\d{2}")
+    spp <- find_species(quantScore[i])
+    m <- find_model(quantScore[i])
+    site <- sites[j]
+  
+    df.summary <- dfi %>% 
+      group_by(time, lifeStage, siteID) %>%
+      summarise(lower95 = quantile(value, 0.025),
+                lower75 = quantile(value, 0.125),
+                median = median(value),
+                mean = mean(value),
+                upper75 = quantile(value, 0.875),
+                upper95 = quantile(value, 0.975), 
+                variance = var(value)) %>% 
+      mutate(model = m,species = spp, start.date = st) %>% 
     ungroup() %>% 
     suppressMessages()
   
-  df.process <- bind_rows(df.process, df.summary)
+   df.process <- bind_rows(df.process, df.summary)
   
-  if(i %% 100 == 0) message(i, " of ", length(quantScore), " complete ", round(i/length(quantScore)*100), "%")
+    if(i %% 10 == 0) message(i, " of ", length(quantScore), " complete ", round(i/length(quantScore)*100), "%")
+  }
+  
+  df.process <- df.process %>% 
+    mutate(mice = if_else(grepl("Mice", model), "Mice", "No mice"),
+           weather = if_else(grepl("Weather", model), "Weather", "No weather"))
+  
+  write_csv(df.process, file = file.path(dir.analysis, paste(site, "allDays.csv", sep = "_")))
+  
+  print(paste(sites[j], "Complete", sep = " "))
 }
-
-df.mutate <- df.process %>% 
-  mutate(mice = if_else(grepl("Mice", model), "Mice", "No mice"),
-         weather = if_else(grepl("Weather", model), "Weather", "No weather"))
-
-
-
-write_csv(df.mutate, file = file.path(dir.analysis, paste0(job.sub, "_allDaysQuants.csv")))
 
 
 
