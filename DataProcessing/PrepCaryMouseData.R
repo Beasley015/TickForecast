a9be102c976e0b355b98df94e2bfed1aadce3c6f
E. Beasley <beasley.em@gmail.com>
2026-06-03 11:18:37 -0400

    clean new cary data

--- DataProcessing/PrepCaryMouseData.R
+++ DataProcessing/PrepCaryMouseData.R
@@ -3,30 +3,27 @@
 
 # Output .csv is an input for the script capture_matrix.R
 
-cary.raw <- read.csv("./Data/cary_mna_1991-2022.csv") %>%
-  select(Grid, Year, Week, MNA, Date, spec)
+library(tidyverse)
+
+cary.raw <- read.csv("./Data/MNA.PL.450m2.by week.2025.csv") 
 
 cary.save <- cary.raw %>%
   # Extract site and plot names
-  mutate(siteID = case_when(str_detect(Grid, "Green") ~ "GREN",
-                          str_detect(Grid, "Henry") ~ "HNRY",
-                          str_detect(Grid, "Tea") ~ "TEA",
+  mutate(siteID = case_when(str_detect(grid, "Green") ~ "GREN",
+                          str_detect(grid, "Henry") ~ "HNRY",
+                          str_detect(grid, "Tea") ~ "TEA",
                           TRUE ~ NA)) %>%
-  mutate(plot = case_when(str_detect(Grid, "Control") ~ "001",
-                          str_detect(Grid, "Experimental") ~ "002",
+  mutate(plot = case_when(str_detect(grid, "C") ~ "001",
+                          str_detect(grid, "E") ~ "002",
                           TRUE ~ NA)) %>%
   unite("plotID", siteID, plot, sep = "_", remove=F) %>%
-  select(-c(Grid, plot)) %>%
+  select(-c(grid, plot)) %>%
   
   # Rename data column to match neon
-  rename("collectDate" = "Date") %>%
+  rename("collectDate" = "date_start") %>%
   mutate(collectDate = as.Date(collectDate, format = "%m/%d/%y")) %>%
   
-  # Rename species column to full name
-  filter(spec == "PL") %>%
-  select(-spec) %>%
-  
   # Remove other columns
-  select(-c(Year, Week))
+  select(-c(year))
 
 write_csv(cary.save, "./Data/cary_mouse_formatted.csv")  
